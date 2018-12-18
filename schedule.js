ff9511551c6fc2259de1714f16587a9098ae06ca
acerise <42525915+acerise85@users.noreply.github.com>
2018-12-17 20:44:57 -0700

    working form

---
+++ schedule.js
@@ -0,0 +1,27 @@
+
+  var config = {
+    apiKey: "AIzaSyC8D579KSMQFVVBkw3rtp-8-sOhpIxlu_Q",
+    authDomain: "train-af789.firebaseapp.com",
+    databaseURL: "https://train-af789.firebaseio.com",
+    projectId: "train-af789",
+    storageBucket: "",
+    messagingSenderId: "637559863661"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+
+$('#add-train-form').on('submit', function(event){
+
+    event.preventDefault();
+
+    var newTrain = {
+        trainName: $('#train-name').val(),
+        destination: $('#destination').val(),
+        firstTrainTime: $('#first-train-time').val(),
+        frequency: $('#frequency').val()
+    }
+    database.ref('/trains').push(newTrain);
+    console.log(newTrain)
+})
